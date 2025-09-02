d87e479236dfc3fc84158b7f5a777ef500fa861c
Shashvatb <shashvat.bharti@gmail.com>
2025-09-02 10:47:39 +0200

    added resuming model

--- main.py
+++ main.py
@@ -9,20 +9,10 @@ import torch.nn.functional as F
 from datetime import datetime
 from tqdm import tqdm
 
-def dice_score_per_class(logits, targets, eps=1e-6):
-    # returns mean dice over classes (excluding background optional)
-    probs = F.softmax(logits, dim=1)
-    onehot = F.one_hot(targets, num_classes=probs.shape[1]).permute(0,4,1,2,3).float()
-    dims = (0,2,3,4)
-    inter = torch.sum(probs * onehot, dims)
-    denom = torch.sum(probs + onehot, dims)
-    dice = (2*inter + eps) / (denom + eps)
-    return dice.mean().item()
-
 
 def train_one_epoch(model, loader, optimizer, criterion, scaler, device, max_norm=1.0):
     model.train()
-    running_loss, running_dice = 0.0, 0.0
+    running_loss = 0.0
     for img, seg, _ in tqdm(loader):
         img, seg = img.to(device), seg.to(device)
         optimizer.zero_grad(set_to_none=True)
@@ -36,28 +26,30 @@ def train_one_epoch(model, loader, optimizer, criterion, scaler, device, max_nor
         scaler.step(optimizer)
         scaler.update()
         running_loss += loss.item()
-        with torch.no_grad():
-            running_dice += dice_score_per_class(logits, seg)
+       
     n = len(loader)
-    return running_loss/n, running_dice/n
+    return running_loss/n
 
 @torch.no_grad()
 def validate(model, loader, criterion, device):
     model.eval()
-    val_loss, val_dice = 0.0, 0.0
+    val_loss = 0.0
     for img, seg, _ in tqdm(loader):
         img, seg = img.to(device), seg.to(device)
         logits = model(img)
         loss = criterion(logits, seg)
         val_loss += loss.item()
-        val_dice += dice_score_per_class(logits, seg)
     n = len(loader)
-    return val_loss/n, val_dice/n
+    return val_loss/n
 
 def main():
     epochs = 10
+    best_model = "best_unet3d.pt"
+    resume = True
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model = UNet3D(n_channels=4, n_classes=4).to(device)
+    if resume:
+        model = model.load_state_dict(torch.load(best_model, weights_only=True))
     criterion = CombinedLoss(weight_dice=0.7, weight_ce=0.3) 
     optimizer = optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-5)
     scaler = GradScaler()
@@ -68,15 +60,15 @@ def main():
     val_loader   = DataLoader(val_ds,   batch_size=1, shuffle=False, num_workers=4, pin_memory=True)
 
     
-    best_dice, best_path = 0.0, "best_unet3d.pt"
+    best_loss = 0.0 
     for epoch in range(1, epochs+1):
         print('epoch: ', epoch)
-        tr_loss, tr_dice = train_one_epoch(model, train_loader, optimizer, criterion, scaler, device)
-        va_loss, va_dice = validate(model, val_loader, criterion, device)
-        print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Epoch {epoch:03d} | Train L {tr_loss:.4f} D {tr_dice:.4f} | Val L {va_loss:.4f} D {va_dice:.4f}")
-        if va_dice > best_dice:
-            best_dice = va_dice
-            torch.save(model.state_dict(), best_path)
+        tr_loss = train_one_epoch(model, train_loader, optimizer, criterion, scaler, device)
+        va_loss = validate(model, val_loader, criterion, device)
+        print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Epoch {epoch:03d} | Train L {tr_loss:.4f} | Val L {va_loss:.4f}")
+        if va_loss > best_loss:
+            best_loss = va_loss
+            torch.save(model.state_dict(), best_model)
 
 
 if __name__ == "__main__":
